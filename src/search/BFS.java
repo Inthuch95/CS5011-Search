c81df22de70a97047c651f4f4747c08e28d87a41
March1703 <it41@st-andrews.ac.uk>
2017-10-22 12:28:03 +0100

    BFS - construct path from initial position to goal

--- src/search/BFS.java
+++ src/search/BFS.java
@@ -2,7 +2,12 @@ package search;
 
 import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 import map.Node;
 
@@ -11,13 +16,17 @@ public class BFS extends Search {
 		super(map);
 	}
 	public void search() {
+		Map<Node, Node> prev = new HashMap<Node, Node>();
+		ArrayList<Node> directions = new ArrayList<Node>();
+		ArrayList<Node> successors = new ArrayList<Node>();
+		Deque<Node> frontier = new ArrayDeque<Node>();
+		ArrayList<Node> explored = new ArrayList<Node>();
 		char[][] map = this.getMap();
 		Node initialNode = this.getStartNode();
 		System.out.println("Start node: " + initialNode);
-		Deque<Node> frontier = new ArrayDeque<Node>();
-		ArrayList<Node> explored = new ArrayList<Node>();
+		
 		frontier.add(initialNode);
-		Node currentNode;
+		Node currentNode = new Node(0, 0);
 		int count = 0;
 		
 		// find Bob
@@ -28,21 +37,32 @@ public class BFS extends Search {
 //			System.out.println("frontier: " + frontier);
 //			System.out.println("explored: " + explored + "\n");
 			if(map[currentNode.getX()][currentNode.getY()] == 'B') {
+				for(Node node = currentNode; node != null; node = prev.get(node)) {
+			        directions.add(node);
+			    }
+				Collections.reverse(directions);
+				count += 1;
 				System.out.println("");
 				System.out.println("Found Bob");
+				System.out.println("Path: " + directions);
 				System.out.println("State explored: " + count + "\n");
 				// reset the states
 				initialNode = currentNode;
 				frontier.clear();
 				explored.clear();
-				count = 0;
+				directions.clear();
+				prev.clear();
 				break;
 			}
-			frontier.addAll(Expand(currentNode, map, frontier, explored));
+			successors = Expand(currentNode, map, frontier, explored);
+			frontier.addAll(successors);
+			for(Node node : successors) {
+				prev.put(node, currentNode);
+			}
 			if (frontier.isEmpty()) {
 				System.out.println("Failed to find Bob :(");
 			}
-			count++;
+			count += 1;
 		}
 		
 		frontier.add(initialNode);
@@ -54,16 +74,26 @@ public class BFS extends Search {
 //			System.out.println("frontier: " + frontier);
 //			System.out.println("explored: " + explored + "\n");
 			if(map[currentNode.getX()][currentNode.getY()] == 'G') {
+				for(Node node = currentNode; node != null; node = prev.get(node)) {
+			        directions.add(node);
+			    }
+				Collections.reverse(directions);
+				count += 1;
 				System.out.println("");
 				System.out.println("Arrived at safe zone");
+				System.out.println("Path: " + directions);
 				System.out.println("State explored: " + count);
 				break;
 			}
-			frontier.addAll(Expand(currentNode, map, frontier, explored));
+			successors = Expand(currentNode, map, frontier, explored);
+			frontier.addAll(successors);
+			for(Node node : successors) {
+				prev.put(node, currentNode);
+			}
 			if (frontier.isEmpty()) {
 				System.out.println("Failed to get to safety :(");
 			}
-			count++;
+			count += 1;
 		}
     }
 	
